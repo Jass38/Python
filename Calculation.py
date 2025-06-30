6f568b4ca0ecba0ae70059f3e3212d7b82dcdd4d
Jaspreet Singh <jaspreetsingh3098@hotmail.com>
2025-06-30 23:59:24 +0530

    Add files via upload

---
+++ Calculation.py
@@ -0,0 +1,12 @@
+#print('Hello world')
+a= int (input('Enter The first Value'))
+b= int (input('Enter the second Value'))
+
+sum = a+b
+print('Addition:',sum)
+sum=a-b
+print('Subtraction:',sum)
+sum= a*b
+print('Multipilcation:',sum)
+sum=a/b
+print('Divide:',sum)
